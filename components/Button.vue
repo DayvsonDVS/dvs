4b3abc4b59c2f5e474a3491af402936f122a7783
DayvsonDvs <dayvson_dvs@hotmail.com>
2022-10-14 18:41:38 -0300

    first commit

---
+++ components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button :style="{ background: color }">
+    <div>
+      <h1>Use o meu component</h1>
+    </div>
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  color: {
+    type: String,
+    required: false,
+    default: '#2fa'
+  }
+})
+</script>
+
+<style scoped lang="scss"></style>
